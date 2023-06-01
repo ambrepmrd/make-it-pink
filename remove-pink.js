3d0336142a52cd36f1547fd740edf9be1a7f771e
ambrepmrd <ambre.pomarede@laposte.net>
2023-06-01 18:20:01 +0200

    Init commit

---
+++ remove-pink.js
@@ -0,0 +1,38 @@
+function removePinkTint(element) {
+  // Récupère les couleurs initiales stockées et les restaure
+  let initialColorBg = element.dataset.initialColorBg;
+  let initialColorText = element.dataset.initialColorText;
+
+  element.style.backgroundColor = initialColorBg;
+  element.style.color = initialColorText;
+
+  // Supprime la casse si elle commence par 'rose-tint-'
+  for (let i = element.classList.length - 1; i >= 0; i--) {
+    let className = element.classList[i];
+    if (className.startsWith('rose-tint-')) {
+      element.classList.remove(className);
+    }
+  }
+}
+
+// Arrête d'observer les mutations du DOM
+function stopObserving() {
+  if (pinkModeObserver) {
+    pinkModeObserver.disconnect();
+  }  
+}
+
+let allElements = document.querySelectorAll('*');
+for (let element of allElements) {
+  removePinkTint(element);
+}
+
+let styleSheet = document.styleSheets[0];
+for (let i = styleSheet.cssRules.length - 1; i >= 0; i--) {
+  let rule = styleSheet.cssRules[i];
+  if (rule.selectorText && rule.selectorText.includes('rose-tint-')) {
+    styleSheet.deleteRule(i);
+  }
+}
+
+stopObserving();
